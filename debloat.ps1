c323c2389901e2fce2104315298bb811d9874f7e
gam3less <73417276+gam3less@users.noreply.github.com>
2020-10-25 14:50:36 +0800

    Update debloat.ps1

--- debloat.ps1
+++ debloat.ps1
@@ -1,16 +1,19 @@
-##########
+# Debloat Windows
+# An edit on Chris Titus Tech's Debloater Script
+#
+#Use the following command to debloat windows:
+#
+#	powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://git.io/JTrqX')"
+#
+#
 # Tweaked Win10 Initial Setup Script
 # Primary Author: Disassembler <disassembler@dasm.cz>
 # Primary Author Source: https://github.com/Disassembler0/Win10-Initial-Setup-Script
 # Tweaked Source: https://gist.github.com/alirobe/7f3b34ad89a159e6daa1/
+# 
+# Note from author: Never run scripts without reading them & understanding what they do.
 #
-#    Note from author: Never run scripts without reading them & understanding what they do.
-#
-#    !!!Addition: One command!!!
-#
-#     > powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://git.io/JTrqX')"
-#
-#	Chris Titus Tech Additions:
+# Chris Titus Tech Additions:
 #
 #	- Dark Mode
 #	- One Command to launch and run
@@ -19,13 +22,13 @@
 #	- Added Install Programs
 #	- Added Debloat Microsoft Store Apps
 #	- Added Confirm Menu for Adobe and Brave Browser
-#	- Changed Default Apps to Notepad++, Brave, Irfanview, and more using XML Import feature (Removed by Gameless due to new browsers)
+#	- Changed Default Apps to Notepad++, Brave, Irfanview, etc (removed by Gam3less due to new browser)
 #
-#	Gameless Additions:
+# Gam3less Additions:
 #
-#	- Pick Browser
-#   - Customized and removed over 1400 lines of code
-##########
+#	- Added an option to pick a browser and install from Brave, Firefox or Google Chrome
+#	- Customized and removed over 1400 lines of code
+#	- Changed one command to run this script
 # Default preset
 $tweaks = @(
 	### Require administrator privileges ###
